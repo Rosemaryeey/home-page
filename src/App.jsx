ae8114fa98f861c7f7019fcbdfa2091bf91eda36
Oguezuonu-Ndubuizu Rosemary Ugonna <rosemaryoguezuonu@gmail.com>
2024-07-16 08:14:02 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Header from "./components/header";
+
 import "./App.css";
 import Main from "./main2";
 import Footer from "./components/footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
+
 import Team from "./pages/team";
 import Pages from "./pages/pages";
 import Partner from "./pages/partner";
@@ -14,11 +14,11 @@ import Shop from "./pages/shop";
 function App() {
   return (
     <div className=" one text-white">
-      {/* <Header /> */}
+
       <Routes>
         <Route path="/" element ={<Main/>}/>
         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
+      
         <Route path="/team" element={<Team />} />
         <Route path="/pages" element={<Pages />} />
         <Route path="/partner" element={<Partner />} />
